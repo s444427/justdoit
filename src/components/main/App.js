fceab57860a7398d8da1dff952830771508d69a3
Lewy <pawlew2@st.amu.edu.pl>
2023-01-14 19:37:20 +0100

    update

--- src/components/main/App.js
+++ src/components/main/App.js
@@ -17,7 +17,7 @@ export const App = (props) => {
             <div style={{paddingTop: "70px", maxWidth: "1500px", margin: "auto", display: "flex"}}>
                     <div style={{background: "#304D6D", paddingTop: "20px", paddingBottom: "20px", height: "800px", width: "30%"}}>
 
-                        <nav style={{height: "750px", overflow: "hidden", overflowY: "scroll"}}>
+                        <nav style={{height: "750px", overflow: "hidden", overflowY: "scroll", }}>
                             {props.yachts_content.length !== 0?
                                 props.yachts_content.map(card =>
                                     <YachtCard key = {card.id} {...props} {...card}/>
